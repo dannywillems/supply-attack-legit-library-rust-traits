e71e50d87e717b936033d3376d7a2d2e4e902386
Danny Willems <be.danny.willems@gmail.com>
2024-01-26 23:00:32 +0100

    Update main for simple exmaple

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,13 @@
-use std::ops::Add;
-
-struct S1 {
-    pub x: u64,
-    pub y: u64,
-}
-
-struct S2 {
-    pub x: u64,
-    pub y: u64,
-    pub z: u64,
-}
-
-impl Add for S1 {
-    type Output = S2;
-
-    fn add(self, rhs: Self) -> Self::Output {
-        S2 {
-            x: self.x + rhs.x,
-            y: self.y + rhs.y,
-            z: self.x + rhs.y,
-        }
-    }
-}
-
-// impl Add for S1 {
-//     type Output = S1;
-
-//     fn add(self, rhs: Self) -> Self::Output {
-//         S1 {
-//             x: self.x + rhs.x,
-//             y: self.y + rhs.y,
-//         }
-//     }
-// }
+use supply_attack_rust_traits_fake_lib::S1;
 
 fn main() {
     let s1 = S1 { x: 1, y: 2 };
     let s2 = S1 { x: 1, y: 2 };
-    let s3 = s1 + s2;
-    let exp_x = 2;
-    let exp_y = 4;
 
-    assert_eq!(exp_x, s3.x);
-    assert_eq!(exp_y, s3.y);
+    // Checking s3 and exp_output are the same.
+    let s3 = s1 + s2;
+    let exp_output = S1 { x: 2, y: 4 };
+
+    assert_eq!(exp_output.x, s3.x);
+    assert_eq!(exp_output.y, s3.y);
 }
